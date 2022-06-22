35fb22a18cbadba1b43fd503a3e254b22c2f9001
Rita-Gomez <ritagomezh@gmail.com>
2022-06-22 17:35:15 -0500

    finished

---
+++ app.js
@@ -0,0 +1,27 @@
+// Contador Inicial
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach( (btn)=>{
+    btn.addEventListener("click", (which)=>{
+    const styles = which.currentTarget.classList;
+        if (styles.contains("decrease")) {
+          count--;
+        } else if (styles.contains("increase")){
+            count++;
+        } else {
+            count = 0;
+        }
+        
+        if (count > 0) {
+            value.style.color = "green";
+        } else if (count < 0){
+            value.style.color = "red";
+        } else {
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    });
+});
